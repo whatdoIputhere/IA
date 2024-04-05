858a9feca149eaae4e3382666617058bbaf283a6
Masterblaze9 <72996460+Masterblaze9@users.noreply.github.com>
2024-04-05 23:41:20 +0100

    deph_limited_search
    Not totally implemented, gives the path but does not stop when the level is exceded and the path is not correct

--- algorithms.py
+++ algorithms.py
@@ -3,8 +3,29 @@ from functions import *
 def uniform_cost(cities, start_city, end_city):
     print("Uniform Cost Search")
     
-def depth_limited_search(cities, start_city, end_city):
-    print("Depth Limited Search")
+def depth_limited_search(cities, start_city, end_city, depth_limit, current_depth=0, path=[], distance=0):
+    if current_depth == depth_limit:
+        print("Depth Limit Exceeded")
+        return None, None
+
+    path.append(start_city.getName())
+
+    for connection in start_city.getConnections():
+        city = next((city for city in cities if city.getName() == connection["name"]), None)
+        if city is None:
+            continue
+        if city.getName() == end_city.getName():
+            distance += int(connection["distance"])  # Make sure distance is an integer
+            print("Path found:", path + [end_city.getName()])
+            print("Distance:", distance)
+            return path + [end_city.getName()], distance
+        else:
+            new_path, new_distance = depth_limited_search(cities, city, end_city, depth_limit, current_depth + 1,
+                                                          path.copy(), distance + int(connection["distance"]))
+            if new_path is not None:
+                return new_path, new_distance
+
+    return None, None
 
 def greedy_search(cities, start_city, end_city, greedypath = [], greedycost = []):
     minHeuristic = 99999;
