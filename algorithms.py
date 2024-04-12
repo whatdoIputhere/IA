49c8f4f4d770d566783d4a88ca175f7432f67ca4
JoelTito123 <pv19908@alunos.estgv.ipv.pt>
2024-04-11 21:00:22 -0700

    Uniform cost
    Uniform cost

--- algorithms.py
+++ algorithms.py
@@ -2,6 +2,46 @@ from functions import *
 
 def uniform_cost(cities, start_city, end_city):
     print("Uniform Cost Search")
+    # print(cities) 
+    print(start_city.getName())
+    print(end_city.getConnections())  
+    print(end_city.getName())  
+    # Priority queue
+    queue = [(0, start_city)]
+    
+    # To keep track of visited cities
+    visited = set()
+    current_city = start_city.getName()
+
+    while queue:
+        # Sort the queue then pop the item with smallest cost
+        queue.sort(reverse=True)
+        cost, current_city = queue.pop()
+        
+       
+        if current_city == end_city:
+            print("Path found with total cost:",cost)
+            return
+        
+        if current_city not in visited:
+            visited.add(current_city)
+            min_distance = int(99999)
+            name_connection = None
+
+
+            for connection in start_city.getConnections():
+                print("conecctions: ", connection)
+                if int(connection["distance"]) < min_distance:
+                    min_distance = int(connection["distance"])
+                    current_city = connection["name"]
+                    print('current_city :', current_city)
+                    queue.append((min_distance, connection["name"]))    
+            visited.add(current_city)       
+
+    print("Path not found")
+    return
+    
+
     
 def depth_limited_search(cities, start_city, end_city, depth_limit, current_depth=0, path=[], distance=0):
     if current_depth == depth_limit:
